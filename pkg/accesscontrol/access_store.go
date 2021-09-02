a74526bf3cbad54efaeabbb8e7625652b6d87112
Ryan Sanna <ryansann@umich.edu>
2021-09-02 16:23:17 -0700

    deterministic hashing

--- pkg/accesscontrol/access_store.go
+++ pkg/accesscontrol/access_store.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 
 	v1 "github.com/rancher/wrangler/pkg/generated/controllers/rbac/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/cache"
 	"k8s.io/apiserver/pkg/authentication/user"
 )
@@ -49,8 +49,8 @@ func (l *AccessStore) AccessFor(user user.Info) *AccessSet {
 		val, ok := l.cache.Get(cacheKey)
 		if ok {
 			as, _ := val.(*AccessSet)
-			hash := getAccessSetHash(user, as)
-			fmt.Printf("CACHE HIT | user: %s, time: %v, hash: %s\n", user.GetName(), time.Now().Unix(), hash)
+			hash := getHash(as)
+			fmt.Printf("CACHE HIT | user: %s, time: %v, key: %s, hash: %s\n", user.GetName(), time.Now().Unix(), cacheKey, hash)
 			return as
 		}
 	}
@@ -65,8 +65,8 @@ func (l *AccessStore) AccessFor(user user.Info) *AccessSet {
 		l.cache.Add(cacheKey, result, 24*time.Hour)
 	}
 
-	hash := getAccessSetHash(user, result)
-	fmt.Printf("CACHE MISS | user: %s, time: %v, hash: %s\n", user.GetName(), time.Now().Unix(), hash)
+	hash := getHash(result)
+	fmt.Printf("CACHE MISS | user: %s, time: %v, key: %s, hash: %s\n", user.GetName(), time.Now().Unix(), cacheKey, hash)
 	return result
 }
 
@@ -86,59 +86,25 @@ func (l *AccessStore) CacheKey(user user.Info) string {
 	return hex.EncodeToString(d.Sum(nil))
 }
 
-type AccessSetPretty struct {
-	ID  string     `json:"id"`
-	Set []SetEntry `json:"set"`
-}
-
-type SetEntry struct {
-	Key               SetKey            `json:"key"`
-	ResourceAccessSet ResourceAccessSet `json:"resourceAccessSet"`
-}
-
-type ResourceAccessSet []Access
-
-type SetKey struct {
-	Verb string               `json:"verb"`
-	GR   schema.GroupResource `json:"groupResource"`
-}
-
-func getAccessSetHash(user user.Info, s *AccessSet) string {
-	as := &AccessSetPretty{
-		ID:  s.ID,
-		Set: []SetEntry{},
-	}
-	for k, v := range s.set {
-		setKey := SetKey{
-			Verb: k.verb,
-			GR:   k.gr,
+func getHash(a *AccessSet) string {
+	var keys []string
+	var accesses []string
+	for gvr, as := range a.set {
+		s := strings.Join([]string{gvr.gr.Group, gvr.verb, gvr.gr.Resource}, "")
+		keys = append(keys, s)
+		for access, granted := range as {
+			astr := strings.Join([]string{access.Namespace, access.ResourceName, strconv.FormatBool(granted)}, "")
+			accesses = append(accesses, astr)
 		}
-		var l []Access
-		for k2 := range v {
-			l = append(l, k2)
-		}
-		sort.Slice(l, func(i, j int) bool {
-			k1 := l[i].Namespace + l[j].ResourceName
-			k2 := l[j].Namespace + l[j].ResourceName
-			return k1 < k2
-		})
-		setEntry := SetEntry{
-			Key:               setKey,
-			ResourceAccessSet: l,
-		}
-		as.Set = append(as.Set, setEntry)
 	}
-	sort.Slice(as.Set, func(i, j int) bool {
-		k1 := as.Set[i].Key.Verb + as.Set[i].Key.GR.Group + as.Set[i].Key.GR.Resource
-		k2 := as.Set[j].Key.Verb + as.Set[j].Key.GR.Group + as.Set[j].Key.GR.Resource
-		return k1 < k2
-	})
-	byts, err := json.Marshal(as)
-	if err != nil {
-		fmt.Println(err)
-	}
+	sort.Strings(keys)
+	sort.Strings(accesses)
+
+	ks := strings.Join(keys, "")
+	acs := strings.Join(accesses, "")
+
 	d := sha256.New()
-	d.Write(byts)
-	hash := hex.EncodeToString(d.Sum(nil))
-	return hash
+	d.Write([]byte(ks))
+	d.Write([]byte(acs))
+	return hex.EncodeToString(d.Sum(nil))
 }
